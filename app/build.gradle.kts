bd34c14d10edd7b2f41cb2c3f00a891fcc6553d5
Barry Deen <barrydeen@protonmail.com>
2026-03-04 14:16:33 -0500

    chore: bump version to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wisp.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.3.6"
+        versionCode = 15
+        versionName = "0.4.0"
     }
 
     buildTypes {
